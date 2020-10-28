9ab90a96289e65f2972c1d863315876481c4c612
Perdente <58706003+Perdente@users.noreply.github.com>
2020-10-28 09:39:17 +0600

    Create Reverse Binary Strings.cpp

---
+++ Strings/Reverse Binary Strings.cpp
@@ -0,0 +1,32 @@
+//https://codeforces.com/contest/1437/problem/B
+
+// Every operation cuts at most 2 pairs 00 or 11
+// Cut two such pairs with every operation
+// As long as there is a 00-pair, there must be a 11-pair as well, unless all of the string
+// but the 00-pair is alternating: 10100101. In this case, we must do one more operation
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+const int N=1e5+5;
+signed main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        string str;
+        cin>>str;
+        int cnt=0,cnt1=0;
+        for(int i=0;i<n;++i)
+        {
+            if(i and str[i]==str[i-1])
+            {
+                str[i]=='1'?cnt++:cnt1++;
+            }
+        }
+        cout<<max(cnt,cnt1)<<endl;
+    }
+	return 0;
+}
