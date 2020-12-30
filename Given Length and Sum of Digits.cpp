93a0a54a51d4a1a05ed97023b64a98ebe80f6d53
Perdente <58706003+Perdente@users.noreply.github.com>
2020-12-30 09:17:49 +0600

    Create Given Length and Sum of Digits.cpp

---
+++ Given Length and Sum of Digits.cpp
@@ -0,0 +1,105 @@
+https://codeforces.com/contest/489/problem/C
+/*A winner is just a loser who tried one more time*/
+
+#include<bits/stdc++.h>
+using namespace std;
+const long long mod = 1e9+7;
+const long long mx  = 1e5+5;
+
+#define inf 			9e18
+#define zero 			-9e18
+#define PI   			acos(-1.0)  // 3.1415926535897932
+#define Case(J) 		printf("Case %lld: %lld\n",++count,J); ///print case
+#define preci           cout<<fixed<<showpoint<<setprecision(20);
+#define max3(a,b,c) 		max(a,max(b,c))
+#define min3(a,b,c) 		min(a,min(b,c))
+#define GCD(a,b) 		__gcd(a,b)
+#define LCM(a,b) 		(a*(b/__gcd(a,b) ))
+#define MP 			make_pair
+#define pb 			push_back
+#define ppb 			pop_back()
+#define pf 			push_front
+#define ppf 			pop_front()
+#define rev(v) 			reverse(v.begin(),v.end());
+#define srt(v) 			sort(v.begin(),v.end());
+#define grtsrt(v) 		sort(v.begin(),v.end(),greater<ll>());
+#define hellyeah 		exit(0);
+#define file 			freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define vmax(V) 		*max_element(V.begin(),V.end());
+#define vmin(V) 		*min_element(V.begin(),V.end());
+#define	debug(a) 		cout<<"*"<<a<<"$"<<endl;
+#define	debug2(a,b) 		cout<<"$"<<a<<" "<<b<<"$"<<endl;
+#define	debug3(a,b,c) 		cout<<"$"<<a<<" "<<b<<" "<<c<<"$"<<endl;
+#define SET(X,D_type)		memset(X, D_type, sizeof(X))
+#define groot(A)		{cout<<A<<endl;return;}
+#define vins(V)			srt(V)V.resize(unique(V.begin(),V.end())-V.begin());
+#define check           	cout<<"Avengers Assemble"<<endl;
+#define	lol 			cout<<"\n";
+#define meem(X) 		cout<<(X?"Yes":"No")<<endl;
+#define joker(V) 		for(auto X:V)cout<<X;cout<<" ";
+#define papiya(Mp) 		for(auto X:Mp)cout<<X.first<<" "<<X.second<<endl;
+#define lp(i,a) 		for(ll i=0; i<a;i++)
+#define LP(it,S)			for(auto it:S)
+#define lp1(i,a,b) 		for(ll i=a; i<=b;i++)
+#define rlp(i,b,a) 		for(ll i=(b);i>=(a);--i) 
+#define Mat(mat,N,M)		lp(i,N){lp(j,M)cout<<mat[i][j]<<" \n"[j==M-1];}
+typedef long long ll;
+typedef long double ld;
+void Loser(){ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
+long long ceil(ll A,ll B){if(A%B==0)return A/B;else return (A/B)+1ll;}
+long long middle(ll a, ll b, ll c) { if ((a <= b && b <= c) || (c <= b && b <= a)) return b;else if ((b <= a && a <= c) || (c <= a && a <= b))return a;else return c; } 
+bool ispalindrom(string s) { ll i=0, j=s.size()-1;while(i<j){if(s[i]!=s[j])return 0;i++,j--;}return 1;}
+//sort(v.begin(),v.end(),as_second);
+bool as_second(const pair<ll,ll> &a, const pair<ll,ll> &b) {return (a.second < b.second); }//sort the vector pair in assending order according to second element
+bool ds_first(const pair<ll,ll> &a, const pair<ll,ll> &b){ return (a.first > b.first);}//sort the vector pair in decending order according to first element
+bool ds_second(const pair<ll,ll> &a, const pair<ll,ll> &b) {return a.second>b.second;}//sort the vector pair in decending order according to second element
+
+typedef deque<ll> Dq;
+typedef set<ll> St;
+typedef map<ll,ll> M;
+typedef vector<ll> V;
+typedef vector<V> VV;
+typedef vector<pair<ll,ll>> VP;
+typedef priority_queue<ll,vector<ll>,greater<ll>> Pq;
+string str,str1,str2,str3,str4,str5,str6,str7;
+char ch,ch1,ch2,ch3;
+bool flag,flag1,flag2;
+
+long long a,b,c,d,e,f,g,h,l,i,j,k,m,n,o,p,q,r,s,t,u,v,w,x,y,z,test,cnt,cnt1,cnt2,cnt3,cnt4,sum,sum1,sum2,sum3,ans,ans1,ans2,ans3,ma,ma1,ma2,ma3,mi,mi1,mi2,mi3,temp,temp1,temp2,temp3,temp4;
+//long double a,b,c,d,e,f,g,h,l,i,j,k,m,n,o,p,q,r,s,t,u,v,w,x,y,z,cnt,cnt1,cnt2,cnt3,cnt4,sum,sum1,sum2,sum3,ans,ans1,ans2,ans3,ma,ma1,ma2,ma3,mi,mi1,mi2,mi3,temp,temp1,temp2,temp3,temp4;
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+void SectumSempra()
+{
+	cin>>n>>m;
+	V v(n),u(n);
+	if((m==0 and n>1) or (m>9*n))groot("-1 -1");
+	s=m;
+	v[0]=1;
+	m--;
+	rlp(i,n-1,0){
+		v[i]+=min(9ll,m);
+		m-=v[i];
+	}
+	lp(i,n){
+		u[i]+=min(9ll,s);
+		s-=u[i];
+	}
+	
+	joker(v)
+	joker(u)
+	lol
+}
+
+int main()
+{
+	Loser();
+	test=1;
+	//cin>>test;
+	while(test--)
+	{
+		SectumSempra();
+	}
+	return 0;
+}
