8f5d7c33c1b31d18b18e168555ba9cbe9d7be5a6
Perdente <58706003+Perdente@users.noreply.github.com>
2021-03-27 22:36:01 +0600

    Create Closest to the Right.cpp

---
+++ Binary Search/Codeforces-edu/Closest to the Right.cpp
@@ -0,0 +1,25 @@
+//https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/C
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+const int N=1e5+6,mod=1e9+7;
+signed main()
+{
+	int n,k;cin>>n>>k;
+	vector<int>v(n);
+	for(auto &i:v)cin>>i;
+	while(k--){
+	    int x;cin>>x;
+	    int l=-1;//v[l]<x
+	    int r=n;//v[r]>=x
+	    while(r>l+1){
+	        int m=(l+r)/2;
+	        if(v[m]<x) l=m;
+	        else r=m;
+	    }
+	    cout<<r+1<<"\n";
+	}
+	
+	return 0;
+}
+
