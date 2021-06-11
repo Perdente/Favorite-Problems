7738f749733b26e827055435f0d0b36bf7bf3487
Loser <58706003+Perdente@users.noreply.github.com>
2021-06-11 14:10:54 +0600

    Update Aibohphobia(LPS).cpp

--- Dynamic Programming/Aibohphobia(LPS).cpp
+++ Dynamic Programming/Aibohphobia(LPS).cpp
@@ -32,3 +32,45 @@ int main()
 		cout<<n-lps(s,n)<<endl;
 	}
 }
+
+/*#include<bits/stdc++.h>
+using namespace std;
+#define mx 6500
+int lookup[mx][mx];
+
+int LCS_len(string m,string n,int M,int N)
+{
+	for(int i=1;i<=M;++i)
+	{
+		for(int j=1;j<=N;++j)
+		{
+			if(m[i-1]==n[j-1])
+			{
+				lookup[i][j]=lookup[i-1][j-1]+1;
+			}
+			else
+			{
+				lookup[i][j]=max(lookup[i-1][j],lookup[i][j-1]);
+			}
+		}
+	}
+	return lookup[M][N];
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t;cin>>t;
+	while(t--){
+		string s,str;cin>>s;
+		str=s;
+		int n=s.size();
+		reverse(str.begin(), str.end());
+		cout<<n-LCS_len(s,str,n,n)<<'\n';
+	}
+}
+*/
