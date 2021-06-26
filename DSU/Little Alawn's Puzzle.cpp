efebb7cf2ce1a0a3ae356598c52fb511d195ddee
Loser <58706003+Perdente@users.noreply.github.com>
2021-06-26 10:53:34 +0600

    Create  Little Alawn's Puzzle.cpp

---
+++ DSU/Little Alawn's Puzzle.cpp
@@ -0,0 +1,88 @@
+//https://codeforces.com/problemset/problem/1534/C
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+const int N=4e5+5,mod=1e9+7;
+int parent[N],depth[N],sizE[N];
+
+int find_set(int v){
+	if(v==parent[v])return v;
+	return parent[v]=find_set(parent[v]);
+}
+
+bool isSame_set(int a,int b){
+	return find_set(a)==find_set(b);
+}
+
+//union by size
+void make_set(int v) {
+    parent[v] = v;
+    sizE[v] = 1;
+}
+
+void union_set(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (sizE[a] < sizE[b])
+            swap(a, b);
+        parent[b] = a;
+        sizE[a] += sizE[b];
+    }
+}
+
+//union by rank based on depth of the tree
+// void make_set(int v){
+// 	parent[v]=v;
+// 	depth[v]=0;
+// }
+
+// void union_set(int a,int b){
+// 	a=find_set(a);
+// 	b=find_set(b);
+// 	if(a!=b){
+// 		if(depth[a]<depth[b])swap(a,b);
+// 		parent[b]=a;
+// 		if(depth[a]==depth[b])depth[a]++;
+// 	}
+// }
+
+
+
+void Malena(){
+  int n;cin>>n;
+  for(int i=1;i<=n;++i)make_set(i);
+  vector<int>v(n),u(n);
+  for(int i=0;i<n;++i)cin>>v[i];
+  	for(int i=0;i<n;++i)cin>>u[i];
+  for(int i=0;i<n;++i){
+  	union_set(v[i],u[i]);
+  }
+  set<int>s;
+  for(int i=1;i<=n;++i){
+  	s.insert(find_set(i));
+  }
+  int cnt=s.size(),ans=1;
+  for(int i=0;i<cnt;++i){
+  	ans=(ans*2)%mod;
+  }
+  cout<<ans<<'\n';
+}
+
+
+
+signed main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t;
+	t=1;
+	cin>>t;
+	while(t--){
+		Malena();
+	}
+	return 0;
+}
