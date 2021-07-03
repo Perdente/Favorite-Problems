fbcd5b5b4b0db97beb772d7ed228145ed40629bb
Loser <58706003+Perdente@users.noreply.github.com>
2021-07-03 21:34:23 +0600

    Create Eastern Exhibition.cpp

---
+++ Geometry/Eastern Exhibition.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/1486/B
+  cin>>n;
+	V x(n),y(n);
+	lp(i,n){
+		cin>>x[i]>>y[i];
+	}
+	srt(x)
+	srt(y)
+	if(n&1)groot(1)
+	cout<<(x[n/2]-x[n/2-1]+1)*(y[n/2]-y[n/2-1]+1);
+	lol
