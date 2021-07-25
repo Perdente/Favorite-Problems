569db94eca73804d329704f5259d06bcf4bbc882
Loser <58706003+Perdente@users.noreply.github.com>
2021-07-26 01:55:45 +0600

    Create Rat in a Maze.cpp

---
+++ Recursion/Rat in a Maze.cpp
@@ -0,0 +1,62 @@
+//https://www.geeksforgeeks.org/rat-in-a-maze-backtracking-2/
+/*
+{1, 0, 0, 0}
+{1, 1, 0, 1}
+{0, 1, 0, 0}
+{1, 1, 1, 1}
+you need to go from (0,0) to (r-1,col-1) in 4 directions.
+Can you travel from (0,0) to (r-1,col-1) only in value 1 in possitions?
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+void Malena(){
+	int r,c;cin>>r>>c;
+	vector<vector<int>>mat(r,vector<int>(c));
+	vector<vector<bool>>visited(r,vector<bool>(c));
+	for(int i=0;i<r;++i){
+		for(int j=0;j<c;++j){
+			cin>>mat[i][j];
+		}
+	}
+
+	auto inside=[&](int x,int y){
+		return 0<=x and x<r and 0<=y and y<c;
+	};
+
+	function<bool(int,int,int,int)>solve=[&](int si,int sj,int ei,int ej)->bool{
+		if(inside(si,sj) and mat[si][sj]==1){
+			visited[si][sj]=true;
+			if(si==ei and ei==ej)return true;
+			vector<pair<int,int>>directions{{1,0},{-1,0},{0,1},{0,-1}};
+			for(pair<int,int>dir:directions){
+				int ni=si+dir.first;
+				int nj=sj+dir.second;
+				if(inside(ni,nj) and mat[ni][nj]==1 and !visited[ni][nj]){
+					return solve(ni,nj,ei,ej);
+				}
+			}
+		}
+		return false;
+	};
+	cout<<solve(0,0,r-1,c-1)<<'\n';
+	
+}
+
+signed main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t;
+	t=1;
+	//cin>>t;
+	while(t--){
+		Malena();
+	}
+	return 0;
+}
