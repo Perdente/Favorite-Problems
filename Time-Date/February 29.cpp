9077c45cf553542a6b0fa3efe768b9ae981cb6ed
Loser <58706003+Perdente@users.noreply.github.com>
2022-01-26 20:37:32 +0600

    Update February 29.cpp

--- Time-Date/February 29.cpp
+++ Time-Date/February 29.cpp
@@ -1,4 +1,5 @@
 // https://lightoj.com/problem/february-29
+// https://www.geeksforgeeks.org/count-of-leap-years-in-a-given-year-range/
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long int
@@ -36,15 +37,22 @@ void malena(){
     int til_first = numLeaps(first), til_second = numLeaps(second);
     // cout << til_first << " " << til_second << "\n";
     int total_leaps = til_second - til_first;
+    // First we count the total number of leap years in range. But....
+    // How to know if the date didn't exceed the Feb 29, huh ????
     if(isleap(first + 1)) {
         bool take = false;
-        if(vs[0] == "January" or vs[0] == "February") {
+        // If initial month is in January then there is high chance we meet Feb 29, right ?? Yeapp😁
+        // But wait why full month of February🤔, cause last day of February, 29th so every day from February gets the pass, right?
+        // Ok, so what about? March, April and so on? They just leave February behind🥺
+        if(vs[0] == "January" or vs[0] == "February") { 
             take = true;
         }
         if(!take) total_leaps--;
     }
     if(isleap(second)) {
         bool take1 = true;
+        // Similar Fashion date until February 28 has no probablity of 29th Feb, right?
+        // But the rest of the months and dates gets a pass😃
         if(vs1[0] == "January") take1 = false;
         else if(vs1[0] == "February") {
             string num = "";
